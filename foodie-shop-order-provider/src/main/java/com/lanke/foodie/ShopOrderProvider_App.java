91326f294165d7ab39ff2dc8ccc67a1feb12522b
owen <2639147439@qq.com>
2019-02-11 10:50:24 +0800

    begin

---
+++ foodie-shop-order-provider/src/main/java/com/lanke/foodie/ShopOrderProvider_App.java
@@ -0,0 +1,17 @@
+package com.lanke.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
+public class ShopOrderProvider_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ShopOrderProvider_App.class, args);
+	}
+}
