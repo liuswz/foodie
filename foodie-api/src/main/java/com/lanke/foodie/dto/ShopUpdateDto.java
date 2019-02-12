01f00b0d3636d9203da2ed2ca4764f3d7f8bc724
liuswz <2639147439@qq.com>
2019-02-12 16:39:43 +0800

    商家信息修改接口 王磊

---
+++ foodie-api/src/main/java/com/lanke/foodie/dto/ShopUpdateDto.java
@@ -0,0 +1,24 @@
+package com.lanke.foodie.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+@Accessors(chain=true)
+public class ShopUpdateDto {
+
+    private Integer id;
+    private String shopName;
+    private String shopAddress;
+    private String shopEmail;
+    private String shopPhone;
+    private Integer payDetailId;
+    private Integer shopStatus;
+    private String createTime;
+
+    private String mchId;
+    private String apiKey;
+}
--- foodie-shop-detail-provider/src/main/java/com/lanke/foodie/dao/DetailDao.java
+++ foodie-shop-detail-provider/src/main/java/com/lanke/foodie/dao/DetailDao.java
@@ -1,6 +1,7 @@
 package com.lanke.foodie.dao;
 
 import com.lanke.foodie.dto.RegistDto;
+import com.lanke.foodie.dto.ShopUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -16,4 +17,8 @@ public interface DetailDao {
     public int checkUsername(@Param("username") String username);
 
     public int checkShopName(@Param("shopName") String shopName);
+
+    public int updateShop(ShopUpdateDto shopUpdateDto);
+
+    public int updatePay(ShopUpdateDto shopUpdateDto);
 }
