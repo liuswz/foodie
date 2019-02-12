4a4374bcc9455f9ba28ade2bb68904190a0ed3fc
owen <2639147439@qq.com>
2019-02-12 13:00:06 +0800

    owen

--- foodie-shop-detail-consumer/src/main/java/com/lanke/foodie/controller/DetailController.java
+++ foodie-shop-detail-consumer/src/main/java/com/lanke/foodie/controller/DetailController.java
@@ -1,4 +1,40 @@
 package com.lanke.foodie.controller;
 
+import com.lanke.foodie.dto.RegistDto;
+import com.lanke.foodie.json.BaseJson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@Slf4j
+@RestController
 public class DetailController {
+
+
+    private static final String REST_URL_PREFIX = "http://FOODIE-SHOPDETAIL";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/consumer/shopdetail/register")
+    public BaseJson regist(RegistDto registDto  ){
+      //  log.info("测试{}，日志级别{}，输出{}", "demo1aaaaaaaaaaaaaaaaaaaaaa", "info", "info level log");
+        BaseJson baseJson = new BaseJson();
+       // log.info("测试{}，日志级别{}，输出{}", registDto.getMchId(), "info", "info level log");
+       // int flag =detailService.regist(registDto);
+
+        int flag = restTemplate.postForObject(REST_URL_PREFIX + "/shopdetail/register", registDto, Integer.class);
+        if(flag > 0){
+            baseJson.setCode(0);
+            baseJson.setResult("注册成功");
+        }else {
+            baseJson.setCode(1);
+            baseJson.setResult("注册失败");
+        }
+        return baseJson;
+    }
+
 }
--- foodie-shop-detail-provider/src/main/java/com/lanke/foodie/controller/DetailController.java
+++ foodie-shop-detail-provider/src/main/java/com/lanke/foodie/controller/DetailController.java
@@ -3,29 +3,33 @@ package com.lanke.foodie.controller;
 import com.lanke.foodie.dto.RegistDto;
 import com.lanke.foodie.json.BaseJson;
 import com.lanke.foodie.service.DetailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class DetailController {
 
 
     @Autowired
     private DetailService detailService;
-
-    @RequestMapping(value = "/shop-detail/register",method = RequestMethod.GET)
-    public BaseJson regist(RegistDto registDto){
-        BaseJson baseJson = new BaseJson();
-        int flag = detailService.regist(registDto);
+//@RequestBody
+    @RequestMapping(value = "/shopdetail/register",method = RequestMethod.POST)
+    public Integer regist(@RequestBody RegistDto registDto  ){
+       // log.info("测试{}，日志级别{}，输出{}", "demo1", "info", "info level log");
+     /*   BaseJson baseJson = new BaseJson();
+        //log.info("测试{}，日志级别{}，输出{}", registDto.getMchId(), "info", "info level log");
+        int flag =;
         if(flag > 0){
             baseJson.setCode(0);
             baseJson.setResult("注册成功");
         }else {
             baseJson.setCode(1);
             baseJson.setResult("注册失败");
-        }
-        return baseJson;
+        }*/
+        return detailService.regist(registDto);
     }
 }
